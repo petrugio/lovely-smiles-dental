27e7f8b3595f6e61a7bd3aed8ac24a5e7f78cda7
petrugio <petruchelban@gmail.com>
2022-10-08 12:09:36 +0000

    Add callout container to homepage

--- dentist/settings.py
+++ dentist/settings.py
@@ -30,7 +30,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ["lovely-smiles-dental.herokuapp.com", "localhost"]
 
