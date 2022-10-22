b91a74012897bb826d512479cea1b93f1e41105a
petrugio <petruchelban@gmail.com>
2022-10-22 10:25:10 +0000

    Add urls to urls.py

--- lovely_smiles/urls.py
+++ lovely_smiles/urls.py
@@ -4,15 +4,27 @@ from . import views
 
 urlpatterns = [
     path(
+        'make_appointment/', views.CreateAppointmentView.as_view(),
+        name='make_appointment'
+        ),
+    path(
+        'appointments/', views.ListAppointmentsView.as_view(),
+        name='appointments'
+        ),
+    path(
+        'edit_appointment/<slug:pk>', views.EditAppointmentsView.as_view(),
+        name='edit_appointment'
+        ),
+    path(
+        'delete_appointment/<slug:pk>', views.DeleteAppointmentView.as_view(),
+        name='delete_appointment'
+        ),
+    path(
         '', views.HomeView.as_view(),
         name='home'
        ),
     path(
         'services', views.ServicesView.as_view(),
         name='services'
-       ),
-    path(
-        'makeappointment/', views.CreateAppointmentView.as_view(),
-        name='makeappointment'
-        )
+       )
 ]
