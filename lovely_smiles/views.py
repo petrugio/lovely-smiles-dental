d0a5fc98f8451be863a116feb94bcd83a71658a3
petrugio <petruchelban@gmail.com>
2022-10-26 02:39:14 +0000

    Add function to display message on  delete

--- lovely_smiles/views.py
+++ lovely_smiles/views.py
@@ -4,6 +4,7 @@ from django.views.generic import DeleteView, CreateView, UpdateView, ListView
 from django.views.generic import TemplateView
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import Appointment
 from .forms import MakeAppointmentForm
 
@@ -112,23 +113,30 @@ class EditAppointmentsView(LoginRequiredMixin,
 
         messages.success(
             self.request,
-            f'Successfully updated appointment for {patient_name} on {date} at {time}'
+            f'Successfully updated appointment , \
+                for {patient_name} on {date} at {time}'
         )
         return super(EditAppointmentsView, self).form_valid(form)
 
     def test_func(self):
-        """ Test user is staff or show 403 """
+        """ Test if the user is the owner or is staff else show 403 """
         if self.request.user.is_staff:
             return True
         else:
             return self.request.user == self.get_object().user
 
 
-class DeleteAppointmentView(LoginRequiredMixin, UserPassesTestMixin,
-                            DeleteView):
+class DeleteAppointmentView(SuccessMessageMixin, LoginRequiredMixin,
+                            UserPassesTestMixin, DeleteView):
     """ A view to delete a appointment """
     model = Appointment
     success_url = "/appointments"
+    success_message = "Appointment deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.warning(self.request, self.success_message)
+        return super(DeleteAppointmentView, self).delete(
+            request, *args, **kwargs)
 
     def test_func(self):
         """ Test if the user is the owner or is staff else show 403 """
