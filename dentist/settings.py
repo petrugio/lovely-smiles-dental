7cfcff2f6b994040255e31e830b48317c3379a5b
petrugio <petruchelban@gmail.com>
2022-10-26 02:35:59 +0000

    Remove commented code

--- dentist/settings.py
+++ dentist/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    
+
 ]
 
 SITE_ID = 1
@@ -88,16 +88,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dentist.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
